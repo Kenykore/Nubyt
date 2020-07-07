85da59ceb5a6fdf1532a742dffa9902303994e2c
Oluwakorede Moshood <kenykore@gmail.com>
2020-07-07 16:53:34 +0100

    updates

--- validations/validate_create_post.js
+++ validations/validate_create_post.js
@@ -2,7 +2,6 @@ const Joi = require("@hapi/joi");
 
 const ValidatePostCreation = (params) => {
     const schema = Joi.object({
-        title: Joi.string().optional(),
         user_id:Joi.string().required(),
         description:Joi.string().required(),
         media_id:Joi.string().required()
